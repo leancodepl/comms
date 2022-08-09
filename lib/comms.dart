74246d6b85a81bc58a5058f7e4f0f7d8eb2bc330
Jan Lewandowski <iasiu@icloud.com>
2022-08-09 14:28:46 +0200

    Remove export of unexisting file

--- lib/comms.dart
+++ lib/comms.dart
@@ -8,8 +8,6 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:logging/logging.dart';
 import 'package:uuid/uuid.dart';
 
-export 'package:comms/widgets/message_listener.dart';
-
 part 'listeners.dart';
 part 'message_sink_register.dart';
 part 'senders.dart';
