9c5b45fc394dee7cd13acbb3f5916949b97ff868
Jan Lewandowski <iasiu@icloud.com>
2022-09-28 10:59:34 +0200

    Add doc comments for MultiListener and ListenerDelegate

--- packages/comms/lib/src/multi_listener.dart
+++ packages/comms/lib/src/multi_listener.dart
@@ -1,5 +1,7 @@
 part of '../comms.dart';
 
+/// Helper class used in [MultiListener] to create [Listener]s of specified 
+/// types.
 class ListenerDelegate<T> with Listener<T> {
   ListenerDelegate();
 
@@ -18,9 +20,23 @@ class ListenerDelegate<T> with Listener<T> {
   void onMessage(T message) => _onMessage(message);
 }
 
+/// A mixin used on classes that want to receive messages of multiple types.
+///
+/// Subclasses must call [listen] when they want to start receiving messages and
+/// [cancel] when they want to stop receiving messages.
+///
+/// Subclasses must implement [onMessage].
+///
+/// See also:
+///
+///  * [Listener], which enables listening to messages of single type.
 mixin MultiListener {
   List<ListenerDelegate> get listenerDelegates;
 
+  /// Starts message receiving.
+  ///
+  /// Registers message sinks based on [listenerDelegates] in
+  /// [MessageSinkRegister].
   @protected
   @nonVirtual
   void listen() => listenerDelegates.forEach(_listen);
@@ -28,9 +44,14 @@ mixin MultiListener {
   void _listen(ListenerDelegate listenerDelegate) =>
       listenerDelegate.._init(onMessage);
 
+  /// Called every time a new [message] of type specified in [listenerDelegates]
+  /// is received.
   @protected
   void onMessage(dynamic message);
 
+  /// Stops receiving messages.
+  ///
+  /// Removes message sinks from [MessageSinkRegister].
   @protected
   @nonVirtual
   void cancel() => listenerDelegates.forEach(_cancel);
